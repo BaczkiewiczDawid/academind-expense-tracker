cd7e231a358734e8c90e9916f19628a222ca08c7
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-02-21 11:20:27 +0100

    section-03

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      title: "New Desk",
+      amount: 345.5,
+      date: new Date(2020, 4, 7),
+    },
+    {
+      id: 3,
+      title: "New Shoes",
+      amount: 98.85,
+      date: new Date(2021, 8, 17),
+    },
+  ];
+
   return (
-    <h1>Hello</h1>
+    <>
+      <Expenses items={expenses} />
+    </>
   );
 }
 
